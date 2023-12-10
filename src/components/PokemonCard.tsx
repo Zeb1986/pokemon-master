cd4d1b939e00ce8a0008e785bc7d97bbdf2ccb46
Viacheslav Tykhonov <Zeb1986@ukr.net>
2023-12-10 22:46:26 +0200

    Fix pokemon page layout

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,6 @@
 'use client'
-import * as React from 'react'
-import useResizeObserver from '@react-hook/resize-observer'
-import {Button, Card, Flex, FloatButton} from "antd";
+import styles from './pokemonCard.module.css'
+import {Button, Card, Flex} from "antd";
 import Image from "next/image";
 import {PokemonFull} from "@/types/types";
 import Link from "next/link";
@@ -10,39 +9,24 @@ interface PokemonCardProps {
     pokemon: PokemonFull
 }
 
-// @ts-ignore
-const useSize = (target) => {
-    const [size, setSize] = React.useState<DOMRectReadOnly>()
-
-    React.useLayoutEffect(() => {
-        setSize(target.current.getBoundingClientRect())
-    }, [target])
-
-    // Where the magic happens
-    useResizeObserver(target, (entry) => setSize(entry.contentRect))
-    return size
-}
-
 const PokemonCard: React.FC<PokemonCardProps> = ({pokemon}) => {
-    const target = React.useRef(null)
-    const size = useSize(target)
     return (
         <div>
             <Flex align="center" gap={"small"} vertical>
                 <Link href="/">
-                    <Button block style={{width: 600}}>Back to pokemons</Button>
+                    <Button style={{maxWidth: 600}}>Back to pokemons</Button>
                 </Link>
                 <Card
                     key={pokemon.id}
-                    ref={target}
                     bodyStyle={{backgroundColor: "lavender"}}
                     style={{maxWidth: 600}}
-                    cover={<Image priority={true}
+                    cover={<div className={styles.imageWrapper}>
+                        <Image priority={true}
                                   alt="picature of pokemon"
                                   src={pokemon.image}
-                                  width={size?.width || 600}
-                                  height={size?.width || 600}
-                    />}>
+                                  fill
+                    />
+                </div>}>
                     <h1>This is {pokemon.name}</h1>
                     <h2>From {pokemon.classification}s</h2>
                     <p>His weight is from {pokemon.weight.minimum} to {pokemon.weight.maximum}</p>
@@ -59,7 +43,6 @@ const PokemonCard: React.FC<PokemonCardProps> = ({pokemon}) => {
                     <p>His maxHP is: {pokemon.maxHP}</p>
                 </Card>
             </Flex>
-            <FloatButton.BackTop visibilityHeight={10}/>
         </div>
 
     )
