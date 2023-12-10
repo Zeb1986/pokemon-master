2f316705423cf490190bee0a5d274d1ae5235144
Viacheslav Tykhonov <Zeb1986@ukr.net>
2023-12-10 20:47:30 +0200

    Add Suspense to card

--- src/components/PokemonsList.tsx
+++ src/components/PokemonsList.tsx
@@ -1,25 +1,27 @@
 'use client'
-import {ChangeEvent, useState} from "react";
-import {Card, Divider, Flex, Pagination, Select, Typography} from "antd";
-import type { SelectProps } from 'antd';
+import {ChangeEvent, useState, Suspense} from "react";
+import {Card, Divider, Flex, Pagination, Select, Spin, Typography} from "antd";
+import type {SelectProps} from 'antd';
 import {Pokemon} from "@/types/types";
 import Image from "next/image";
 import Link from 'next/link'
 import Search from "antd/es/input/Search";
+
 interface PokemonsListProps {
     pokemons: Pokemon[];
 }
+
 const PokemonsList = ({pokemons = []}: PokemonsListProps) => {
     const [currentPage, setCurrentPage] = useState<number>(1)
     const [classes, setClasses] = useState<string[]>([]);
     const [nameSearch, setNameSearch] = useState<string>('');
-    const itemsPerPage =10;
+    const itemsPerPage = 10;
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const { Title } = Typography;
+    const {Title} = Typography;
     const options: SelectProps['options'] = []
     //Creating pokemons class options for filtering
-    new Set(pokemons.map((pokemon:Pokemon) => pokemon.classification ))
+    new Set(pokemons.map((pokemon: Pokemon) => pokemon.classification))
         .forEach(value => options.push({
             label: value,
             value,
@@ -29,58 +31,65 @@ const PokemonsList = ({pokemons = []}: PokemonsListProps) => {
             return classes.includes(value.classification)
         } else return true
     })
-  return (
-      <>
-          <Flex vertical align="center">
-              <Title level={4}>Search Pokemon By Name</Title>
-              <Search
-                  placeholder="Search Pokemon By Name"
-                  value={nameSearch}
-                  allowClear
-                  onChange={(e:ChangeEvent<HTMLInputElement>) => setNameSearch(e.target.value.trim().toLowerCase())}
-                  style={{ width: 300 }} />
-              <Title level={4}>Select Pokemon&apos;s Classes</Title>
-              <Select
-                  mode="multiple"
-                  style={{ minWidth: 300 }}
-                  placeholder="Select Pokemons Classes"
-                  allowClear
-                  onChange={value => setClasses(value)}
-                  options={options}
-              />
-          </Flex>
-          <Divider orientation="center">Pokemon&apos;s</Divider>
-          <Flex wrap="wrap" gap="large" justify="center">
-              {pokemons && pokemonsToMap
-                  //cut pokemons for pages
-                  .slice(startIndex, endIndex)
-                  .map((pokemon:Pokemon) => {
-                  const { Meta } = Card;
-                  return (
-                      <Link key={pokemon.id} href={`/pokemons/${pokemon.id}`}>
-                          <Card
-                              key={pokemon.id}
-                              bodyStyle={{backgroundColor: "lavender"}}
-                              hoverable
-                              style={{ width: 300 }}
-                              cover={<Image priority={true} alt="picature of pokemon" src={pokemon.image} width={300} height={300} />}
-                          >
-                              <Meta title={pokemon.name} description={pokemon.classification} />
-                          </Card>
-                      </Link>
+    return (
+        <>
+            <Flex vertical align="center">
+                <Title level={4}>Search Pokemon By Name</Title>
+                <Search
+                    placeholder="Search Pokemon By Name"
+                    value={nameSearch}
+                    allowClear
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setNameSearch(e.target.value.trim().toLowerCase())}
+                    style={{width: 300}}/>
+                <Title level={4}>Select Pokemon&apos;s Classes</Title>
+                <Select
+                    mode="multiple"
+                    style={{minWidth: 300}}
+                    placeholder="Select Pokemons Classes"
+                    allowClear
+                    onChange={value => setClasses(value)}
+                    options={options}
+                />
+            </Flex>
+            <Divider orientation="center">Pokemon&apos;s</Divider>
+            <Flex wrap="wrap" gap="large" justify="center">
+                {pokemons && pokemonsToMap
+                    //cut pokemons for pages
+                    .slice(startIndex, endIndex)
+                    .map((pokemon: Pokemon) => {
+                        const {Meta} = Card;
+                        return (
+                            <Link key={pokemon.id} href={`/pokemons/${pokemon.id}`}>
+                                <Card
+                                    key={pokemon.id}
+                                    bodyStyle={{backgroundColor: "lavender"}}
+                                    hoverable
+                                    style={{width: 300}}
+                                    cover={<Suspense fallback={
+                                        <Spin tip="Loading" size="large">
+                                            <div className="content"/>
+                                        </Spin>}>
+                                        <Image priority={true} alt="picature of pokemon" src={pokemon.image} width={300}
+                                               height={300}/>
+                                    </Suspense>
+                                    }
+                                >
+                                    <Meta title={pokemon.name} description={pokemon.classification}/>
+                                </Card>
+                            </Link>
 
-                  )
-              })}
-          </Flex>
-          <Divider/>
-          <Flex justify="center">
-              <Pagination
-                  current={currentPage}
-                  showSizeChanger={false}
-                  onChange={(page) => setCurrentPage(page)}
-                  total={pokemonsToMap.length} />
-          </Flex></>
-  )
+                        )
+                    })}
+            </Flex>
+            <Divider/>
+            <Flex justify="center">
+                <Pagination
+                    current={currentPage}
+                    showSizeChanger={false}
+                    onChange={(page) => setCurrentPage(page)}
+                    total={pokemonsToMap.length}/>
+            </Flex></>
+    )
 }
 
 export default PokemonsList
