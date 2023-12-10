682eab50c4d5fbb3e99f982f0f95c1ef84da6bb1
Viacheslav Tykhonov <Zeb1986@ukr.net>
2023-12-10 21:00:32 +0200

    delete Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { makeRequest} from "@/helpers/requests";
 import { GET_POKEMONS} from "@/helpers/querys";
 import {Pokemon} from "@/types/types";
 import PokemonsList from "@/components/PokemonsList";
-import {Suspense} from "react";
-import {Spin} from "antd";
 const getPokemons = async () => {
   const variables = {
     first: 100
@@ -14,12 +12,7 @@ export default async function Home() {
   const pokemons:[Pokemon] = await getPokemons().then(res => res.data.pokemons)
   return (
       <main>
-          <Suspense fallback={
-              <Spin tip="Loading" size="large">
-                  <div className="content"/>
-              </Spin>}>
         <PokemonsList pokemons={pokemons}/>
-              </Suspense>
       </main>
   )
 }
